2a587bdba460f38ede2d2a6ce16e5fbfd399214c
Ivan Moll <ivanmoll07@gmail.com>
2022-11-21 22:44:31 +0100

    feat: add middlewares

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@
 const express = require('express')
 const config = require('./config')
 const MongoDBClient = require('./db')
+const middlewares = require('./middlewares')
 const PORT = config.incomingPort
 
 const app = express()
 
 const connect = async () => await new MongoDBClient().connect()
 connect()
+
+middlewares.init(app)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
