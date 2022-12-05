eca0295e55ccb8fca66b642fb999dcb7129d8b2b
Ivan Moll <ivanmoll07@gmail.com>
2022-12-05 15:26:25 +0100

    feat: add start for STG and PROD envs

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,36 @@
-const { parsed } = require('dotenv').config()
-const bcrypt = require('bcrypt')
+'use strict'
 
-module.exports = {
-    mongoUrl:               parsed.MONGO_URL,
-    incomingPort:           parsed.API_INCOMING_PORT,
-    tokenSecret:            parsed.TOKEN_SECRET,
-    refreshTokenSecret:     parsed.REFRESH_TOKEN_SECRET,
-    tokenExpTime:           String(parsed.TOKEN_EXP_TIME),
-    refreshTokenExpTime:    String(parsed.REFRESH_TOKEN_EXP_TIME),
-    bcryptSalt:             Number(parsed.BCRYPT_SALT),
-    superAdminEmail:        parsed.SUPERADMIN_EMAIL,
-    superAdminPass:         bcrypt.hashSync(parsed.SUPERADMIN_PASS, bcrypt.genSaltSync(Number(parsed.BCRYPT_SALT))),
-    limitBody:              parsed.API_LIMIT_CONTENT_LENGTH
+const bcrypt = require('bcrypt')
+const env = process.env.NODE_ENV
+let envVars
+
+if (env === 'prod' || env === 'stg') {
+    envVars = {
+        mongoUrl:               process.env.MONGO_URL,
+        incomingPort:           process.env.API_INCOMING_PORT,
+        tokenSecret:            process.env.TOKEN_SECRET,
+        refreshTokenSecret:     process.env.REFRESH_TOKEN_SECRET,
+        tokenExpTime:           String(process.env.TOKEN_EXP_TIME),
+        refreshTokenExpTime:    String(process.env.REFRESH_TOKEN_EXP_TIME),
+        bcryptSalt:             Number(process.env.BCRYPT_SALT),
+        superAdminEmail:        process.env.SUPERADMIN_EMAIL,
+        superAdminPass:         bcrypt.hashSync(process.env.SUPERADMIN_PASS, bcrypt.genSaltSync(Number(process.env.BCRYPT_SALT))),
+        limitBody:              process.env.API_LIMIT_CONTENT_LENGTH
+    }
+} else {
+    const { parsed } = require('dotenv').config()
+    envVars = {
+        mongoUrl:               parsed.MONGO_URL,
+        incomingPort:           parsed.API_INCOMING_PORT,
+        tokenSecret:            parsed.TOKEN_SECRET,
+        refreshTokenSecret:     parsed.REFRESH_TOKEN_SECRET,
+        tokenExpTime:           String(parsed.TOKEN_EXP_TIME),
+        refreshTokenExpTime:    String(parsed.REFRESH_TOKEN_EXP_TIME),
+        bcryptSalt:             Number(parsed.BCRYPT_SALT),
+        superAdminEmail:        parsed.SUPERADMIN_EMAIL,
+        superAdminPass:         bcrypt.hashSync(parsed.SUPERADMIN_PASS, bcrypt.genSaltSync(Number(parsed.BCRYPT_SALT))),
+        limitBody:              parsed.API_LIMIT_CONTENT_LENGTH
+    }
 }
+
+module.exports = envVars
