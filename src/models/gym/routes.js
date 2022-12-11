27787d6ec2e79bbeb81513263c5f0149dd089d52
Ivan Moll <ivanmoll07@gmail.com>
2022-12-11 16:01:16 +0100

    Unfix bug

--- src/models/gym/routes.js
+++ src/models/gym/routes.js
@@ -5,10 +5,7 @@ const router = express.Router()
 const gymController = require('./controller')
 const middlewares = require('../../middlewares')
 
-router.post('/', [
-    middlewares.auth,
-    middlewares.isAdmin
-], gymController.create)
+router.post('/', middlewares.isAdmin, gymController.create)
 router.get('/:id', gymController.getById)
 router.get('/', gymController.getAll)
 router.put('/:id', [
